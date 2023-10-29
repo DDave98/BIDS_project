865438cc3a15032703689021c01e4fa9e6ee479f
Adam Pech <93526032+P3chys@users.noreply.github.com>
2023-10-29 18:10:24 +0100

    Added more cases

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ from data_objects import Table as tb
 
 #create client for specific dataset and table
 stock_client = db(ds.STOCKS,tb.STOCK_DATA_LIVE)
+forex_client = db(ds.FOREX,tb.FOREX_DATA_LIVE)
 
 """
 call functions from DefaultOperations using client created above
 data can be printed directly or in cycle if more than 1 result is present
 """
-data = ops.get_schema(stock_client.table)
+#get schema of table
+stock_schema = ops.get_schema(stock_client.table)
+print(stock_schema)
+
+forex_schema = ops.get_schema(forex_client.table)
+print(forex_schema)
+
+#select * from table
 data = ops.get_data(stock_client.client,stock_client.table)
 
+#query data
 data = ops.query(
     client = stock_client.client,
     i_query = f"SELECT * FROM {stock_client.table}"
     )
-for row in data:
-    print(row)
