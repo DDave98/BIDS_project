bdd420f911c92148b02b97f3da44c1917f49896f
Adam Pech <93526032+P3chys@users.noreply.github.com>
2023-10-29 18:11:03 +0100

    Data objects now reflect all datasets and tables

--- data_objects.py
+++ data_objects.py
@@ -5,7 +5,11 @@ class Dataset(StrEnum):
     STOCKS = "stocks"
     FOREX = "forex"
     COMMODITIES = "commodities"
+    NEWS = "news"
 
 #enum containing all tables in DB
 class Table(StrEnum):
     STOCK_DATA_LIVE = "stock_data_live"
+    FOREX_DATA_LIVE = "forex_data_live"
+    COMMODITIES_DATA_LIVE = "commodities_data_live"
+    NEWS = "news"
